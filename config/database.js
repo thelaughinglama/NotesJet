556c2c72f417f263db7688063f214617db21ae44
prashant <prashantj724@gmail.com>
2019-05-22 05:19:40 +0530

    but fi

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV==='production'){
     module.exports={mongoURI:
-        "mongodb://prashant:111111a@ds159546.mlab.com:59546/notesjet"
+        "mongodb://<ohduck1>:<ohduck1>@ds159546.mlab.com:59546/notesjet"
     }
 }
 else{
